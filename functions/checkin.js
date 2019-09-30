58558fff4373398f9392f662e6b2699fed73055a
James Benns <hi@jamesbenns.com>
2019-09-30 16:27:28 +0100

    update function

--- functions/checkin.js
+++ functions/checkin.js
@@ -17,18 +17,21 @@ exports.handler = function(event, context) {
     const messageSearchTerm = 'Content-Disposition: form-data; name="text"';
     const messageSection = mail.substr(mail.indexOf(messageSearchTerm) + messageSearchTerm.length);
     const message = messageSection.substr(0, messageSection.indexOf('--xYzZY')).trim();
-
-    console.log(message);
     const lat = message.split('lat: ')[1].split(' ')[0];
     const lon = message.split('lon: ')[1].split(' ')[0];
     const date = new Date();
-    const doc = 
-`---
+    const doc = `---
 title: Auto check-in
 date: ${date.toISOString()}
 lat: '${lat}'
 lon: '${lon}'
 ---`;
+
+    const firstNotification = new OneSignal.Notification({      
+        template_id: "726887ee-8f4f-4eaf-bc13-09e96864e467",
+        included_segments: ["Subscribed Users"]     
+    });
+    myClient.sendNotification(firstNotification);
 
     axios({
         method: 'post',
@@ -46,12 +49,4 @@ lon: '${lon}'
             "commit_message": "Auto check-in"
         }
     });
-
-    const firstNotification = new OneSignal.Notification({      
-        template_id: "726887ee-8f4f-4eaf-bc13-09e96864e467",
-        included_segments: ["Subscribed Users"]     
-    });
-    myClient.sendNotification(firstNotification);
 }
-
-// curl --request POST --header 'PRIVATE-TOKEN: 864PF-QH7xmgtZcA1aub' --header "Content-Type: application/json" --data '{"branch": "master", "encoding": "base64", "author_email": "satphone@jamesbenns.com", "author_name": "Sat Phone", "content": "Ci0tLQp0aXRsZTogQXV0byBjaGVjay1pbgpkYXRlOiAyMDE5LTA5LTI2VDIxOjMwOjAwLjAwOFoKbGF0OiAnMzguNzMwODM2Jwpsb246ICcxOS44NDkxMjgnCi0tLQo=", "commit_message": "Auto check-in"}' 'https://gitlab.com/api/v4/projects/14462796/repository/files/content%2Fblog%2F2019-09-26-21-30.md'
