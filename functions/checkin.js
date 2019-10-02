f2cf769e55dc2ceb416d10606e88d943ab32f36b
James Benns <hi@jamesbenns.com>
2019-10-02 15:56:56 +0100

    updates for sat phone

--- functions/checkin.js
+++ functions/checkin.js
@@ -1,17 +1,16 @@
 const axios = require('axios');
 
 exports.handler = function(event, context) {
+
     const parts = event.body.split('+');
     const mail = parts.map(part => {
         const buff = new Buffer(part, 'base64');
         return buff.toString('ascii');
     }).join(' ');
 
-    const messageSearchTerm = 'Content-Disposition: form-data; name="text"';
-    const messageSection = mail.substr(mail.indexOf(messageSearchTerm) + messageSearchTerm.length);
-    const message = messageSection.substr(0, messageSection.indexOf('--xYzZY')).trim();
-    const lat = message.split('lat: ')[1].split(' ')[0];
-    const lon = message.split('lon: ')[1].split(' ')[0];
+    const latStart = 'sent this message from: Lat ';
+    const lat = mail.substr(mail.indexOf(latStart) + latStart.length).split(' ')[0];
+    const lon = mail.substr(mail.indexOf(latStart) + latStart.length).split(' ')[2].split('\n')[0];
     const date = new Date();
 
     const doc = `---
@@ -21,20 +20,23 @@ lat: '${lat}'
 lon: '${lon}'
 ---`;
 
-    axios({
-        method: 'post',
-        headers: {
-            'PRIVATE-TOKEN': '864PF-QH7xmgtZcA1aub',
-            'Content-Type': 'application/json'
-        },
-        url: `https://gitlab.com/api/v4/projects/14462796/repository/files/content%2Fblog%2F${date.getFullYear()}-${("0" + (date.getMonth() + 1)).slice(-2)}-${("0" + date.getDate()).slice(-2)}-${("0" + (date.getHours() + 1)).slice(-2)}-${("0" + (date.getMinutes() + 1)).slice(-2)}.md`,
-        data: {
-            "branch": "master",
-            "encoding": "base64",
-            "author_email": "satphone@jamesbenns.com",
-            "author_name": "Sat Phone",
-            "content": Buffer.from(doc).toString('base64'),
-            "commit_message": "Auto check-in"
-        }
-    });
+    if(!!lat && !!lon) {
+        axios({
+            method: 'post',
+            headers: {
+                'PRIVATE-TOKEN': '864PF-QH7xmgtZcA1aub',
+                'Content-Type': 'application/json'
+            },
+            url: `https://gitlab.com/api/v4/projects/14462796/repository/files/content%2Fblog%2F${date.getFullYear()}-${("0" + (date.getMonth() + 1)).slice(-2)}-${("0" + date.getDate()).slice(-2)}-${("0" + (date.getHours() + 1)).slice(-2)}-${("0" + (date.getMinutes() + 1)).slice(-2)}.md`,
+            data: {
+                "branch": "master",
+                "encoding": "base64",
+                "author_email": "satphone@jamesbenns.com",
+                "author_name": "Sat Phone",
+                "content": Buffer.from(doc).toString('base64'),
+                "commit_message": "Auto check-in"
+            }
+        });
+    }
+
 }
